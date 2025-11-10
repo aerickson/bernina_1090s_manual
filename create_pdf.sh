b1a6f9953dc9300ddbb087bbbb70b490a03ec79c
Andrew Erickson <aerickson@gmail.com>
2025-11-09 22:03:36 -0800

    helper: output to a name similar to the original file

--- create_pdf.sh
+++ create_pdf.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 output_dir="out/export/pic/"
-output_file="bernina_1090s_manual.pdf"
+output_file="BERNINA-Model-1090-S-Manual-Cleaned.pdf"
 
 magick ${output_dir}*.tif ${output_file}
